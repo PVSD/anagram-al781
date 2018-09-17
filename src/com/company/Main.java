8522c4a103f83210d670aa907fa8eb145727f0e5
Alexander <al781@pvsd.org>
2018-09-17 11:33:50 -0400

    Lunch Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,45 @@
 package com.company;
+import java.util.*;
 
-public class Main {
+public class Main
+    {
 
-    public static void main(String[] args) {
-	// write your code here
+        public static void main(String[] args)
+            {
+                Scanner strscan = new Scanner(System.in);
+                String str1;
+                String str2;
+                char[] str1array;
+                char[] str2array;
+                System.out.println("Enter First Word.");
+                str1 = strscan.nextLine();
+                System.out.println("Enter Second Word.");
+                str2 = strscan.nextLine();
+                str1array = str1.toLowerCase().toCharArray();
+                str2array = str2.toLowerCase().toCharArray();
+                Arrays.sort(str1array);
+                Arrays.sort(str2array);
+                boolean isAnagram = false;
+                if (str1.length() != str2.length())
+                    {
+                        isAnagram = false;
+                    }
+                else
+                    {
+
+                        for (int i = 0; i < str1.length(); i++) {
+
+                            if (str1array[i] != str2array[i])
+                                {
+                                    isAnagram = false;
+                                }
+                            else
+                                {
+                                    isAnagram = true;
+                                }
+
+                    }
+                }
+                System.out.println(isAnagram);
+            }
     }
-}
